a9578601e4c1175862d2659583d42030cba128f9
Виктор Билин <vaverkax@gmail.com>
2021-12-21 12:49:48 +0300

    fragment added

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -5,17 +5,24 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import com.example.calculator.databinding.ActivityMainBinding
+import com.example.calculator.databinding.FragmentTextBinding
 import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
     private lateinit var binding: ActivityMainBinding
     private val calculator = CommandProcessor()
+    private val textFragment = TextFragment()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+        supportFragmentManager
+            .beginTransaction()
+            .add(R.id.fragmentContainer, textFragment)
+            .addToBackStack(null)
+            .commit()
 
         val btn = arrayOfNulls<Button>(10)
         for (i in 0..9) {
@@ -23,87 +30,73 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
             val btnId = resources.getIdentifier(btnName, "id", packageName)
             btn[i] = findViewById(btnId)
             btn[i]?.setOnClickListener {
-                when (binding.txtInput.text.isNullOrEmpty()) {
-                    true -> binding.txtInput.text = i.toString()
+                when (textFragment.getText().isNullOrEmpty()) {
+                    true -> textFragment.setText(i.toString())
                     else -> {
-                        when (binding.txtInput.text.toString().first() == '0') {
+                        when (textFragment.getText().first() == '0') {
                             true -> {
-                                when (binding.txtInput.text.count()) {
+                                when (textFragment.getText().count()) {
                                     1 -> {
                                         when (i) {
-                                            0 -> { binding.txtInput.text = "0" }
-                                            else -> { binding.txtInput.text = i.toString() }
+                                            0 -> { textFragment.setText("0") }
+                                            else -> { textFragment.setText(i.toString()) }
                                         }
                                     }
-                                    else -> { binding.txtInput.text = binding.txtInput.text.toString() + i }
+                                    else -> { textFragment.setText(textFragment.getText() + i) }
                                 }
                             }
-                            else -> { binding.txtInput.text = binding.txtInput.text.toString() + i }
+                            else -> { textFragment.setText(textFragment.getText() + i) }
                         }
                     }
                 }
-                setNeededTextSize()
+                textFragment.setNeededTextSize()
             }
         }
 
         binding.btnMultiply.setOnClickListener {
-            binding.txtInput.text = binding.txtInput.text.toString() + "*"
-            setNeededTextSize()
+            textFragment.setText(textFragment.getText() + "*")
+            textFragment.setNeededTextSize()
         }
 
         binding.btnDivide.setOnClickListener {
-            binding.txtInput.text = binding.txtInput.text.toString() + "/"
-            setNeededTextSize()
+            textFragment.setText(textFragment.getText() + "/")
+            textFragment.setNeededTextSize()
         }
 
         binding.btnDot.setOnClickListener {
-            binding.txtInput.text = binding.txtInput.text.toString() + "."
-            setNeededTextSize()
+            textFragment.setText(textFragment.getText() + ".")
+            textFragment.setNeededTextSize()
         }
 
         binding.btnSubtract.setOnClickListener {
-            binding.txtInput.text = binding.txtInput.text.toString() + "-"
-            setNeededTextSize()
+            textFragment.setText(textFragment.getText() + "-")
+            textFragment.setNeededTextSize()
         }
 
         binding.btnAdd.setOnClickListener {
-            binding.txtInput.text = binding.txtInput.text.toString() + "+"
-            setNeededTextSize()
+            textFragment.setText(textFragment.getText() + "+")
+            textFragment.setNeededTextSize()
         }
 
         binding.btnDel.setOnClickListener {
-            binding.txtInput.text = binding.txtInput.text.toString().dropLast(1)
-            setNeededTextSize()
+            textFragment.setText(textFragment.getText().dropLast(1))
+            textFragment.setNeededTextSize()
         }
 
         binding.btnEqual.setOnClickListener {
-            val (result, message) = calculator.startOperations(calculator,binding.txtInput.text.toString())
+            val (result, message) = calculator.startOperations(calculator,textFragment.getText())
             when (result) {
                 null -> {
                     Snackbar.make(view, message,Snackbar.LENGTH_SHORT)
                         .setAction("OK") { }
                         .show()
-                    binding.txtInput.text = "0"
+                    textFragment.setText("0")
                 }
                 else -> {
-                    binding.txtInput.text = result.toPlainString()
+                    textFragment.setText(result.toPlainString())
                 }
             }
-            setNeededTextSize()
-        }
-    }
-
-    private fun setNeededTextSize() {
-        when {
-            binding.txtInput.text.length >= 10 -> {
-                binding.txtInput.textSize = 30f
-            }
-            binding.txtInput.text.length >= 6 -> {
-                binding.txtInput.textSize = 35f
-            }
-            else -> {
-                binding.txtInput.textSize = 48f
-            }
+            textFragment.setNeededTextSize()
         }
     }
 }
